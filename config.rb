48be064f993d7757bf291845660cf1343a8213a6
Matt Rogish <matt.rogish@gmail.com>
2012-08-15 12:16:55 -0400

    Updating source location

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/techblog/"
-http_images_path = "/techblog/images"
-http_fonts_path = "/techblog/fonts"
-css_dir = "public/techblog/stylesheets"
+http_path = "/"
+http_images_path = "/images"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
